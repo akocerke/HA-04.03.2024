f471630fd0a624f5e7827a8d64e5adcbf46a9b82
akocerke <152269613+akocerke@users.noreply.github.com>
2024-03-05 12:25:50 +0100

    describe it implementiert

--- unit-testing/multiply.test.js
+++ unit-testing/multiply.test.js
@@ -3,14 +3,29 @@
 const calculator = require('./calculator');
 
 // Tests für multiply(a, b)
-test("Teste multiply(a, b) mit positiven Zahlen:", () => {
-    expect(calculator.multiply(5, 3)).toBe(15);
-});
+// test("Teste multiply(a, b) mit positiven Zahlen:", () => {
+//     expect(calculator.multiply(5, 3)).toBe(15);
+// });
 
-test("Teste multiply(a, b) mit negativem 'a':", () => {
-    expect(calculator.multiply(-5, 3)).toBe(-15);
-});
+// test("Teste multiply(a, b) mit negativem 'a':", () => {
+//     expect(calculator.multiply(-5, 3)).toBe(-15);
+// });
 
-test("Teste multiply(a, b) mit 'b' gleich 0:", () => {
-    expect(calculator.multiply(5, 0)).toBe(0);
-});
+// test("Teste multiply(a, b) mit 'b' gleich 0:", () => {
+//     expect(calculator.multiply(5, 0)).toBe(0);
+// });
+
+describe('Arithmetische Operationen', () => {
+    it('Teste multiply(a, b) mit positiven Zahlen', () => {
+      expect(calculator.multiply(5, 3)).toBe(15);
+    });
+  
+    it('Teste multiply(a, b) mit negativem "a"', () => {
+      expect(calculator.multiply(-5, 3)).toBe(-15);
+    });
+  
+    it('Teste multiply(a, b) mit "b" gleich 0', () => {
+      expect(calculator.multiply(5, 0)).toBe(0);
+    });
+  });
+  
